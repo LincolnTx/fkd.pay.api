de6c79babf2bcbfdac489a06e65434bf524bc9fc
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-09-07 11:33:50 -0300

    feat: preparing data layer
    Create ApplicationDbContext.cs and config it to use
    Create base classes like Entity and IUnitOfWork.cs
    Add necessary nuget pkg

---
+++ src/fkd.pay.api/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using fkd.pay.api.Domain.SeedWork;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace fkd.pay.api.Data.Context
+{
+    public class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        private readonly IMediator _mediator;
+        private IDbContextTransaction _currentTransaction;
+        public bool HasActiveTransaction => _currentTransaction != null;
+        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+
+        public ApplicationDbContext() { }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IMediator mediator) : base(options)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString"),
+                npgsqlOptionsAction: pgOptions =>
+                {
+                    pgOptions.EnableRetryOnFailure(maxRetryCount: 3, maxRetryDelay: TimeSpan.FromSeconds(30), errorCodesToAdd: null);
+                }
+        );
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Dispatch Domain Events collection. 
+            // Choices:
+            // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+            // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+            await _mediator.DispatchDomainEventsAsync(this);
+
+            // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+            // performed through the DbContext will be committed
+            return await base.SaveChangesAsync(cancellationToken) > 0;
+        }
+    }
+}
