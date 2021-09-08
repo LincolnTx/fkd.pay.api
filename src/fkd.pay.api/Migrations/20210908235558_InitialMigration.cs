4893a6d6845d3fe28e1321b2c955fd5dbcf9469f
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-09-08 20:59:29 -0300

    feat: create initial migration

---
+++ src/fkd.pay.api/Migrations/20210908235558_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fkd.pay.api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "payment_card",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    card_number = table.Column<string>(type: "text", nullable: false),
+                    exp_month = table.Column<int>(type: "integer", nullable: false),
+                    exp_year = table.Column<int>(type: "integer", nullable: false),
+                    card_holder_name = table.Column<string>(type: "text", nullable: false),
+                    available_balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    cvv = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payment_card", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "payment_card");
+        }
+    }
+}
