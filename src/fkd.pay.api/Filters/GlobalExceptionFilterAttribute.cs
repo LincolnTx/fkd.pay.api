459523c46e133394a1272c098a180677327f169e
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-09-08 22:43:31 -0300

    chore: create a global exception handler

---
+++ src/fkd.pay.api/Filters/GlobalExceptionFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using fkd.pay.api.Filters.ErrorModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace fkd.pay.api.Filters
+{
+    public class GlobalExceptionFilterAttribute : Attribute, IExceptionFilter
+    {
+        public GlobalExceptionFilterAttribute() { }
+
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new BadRequestObjectResult(
+                new DefaultError(false, 
+                    new ErrorsResponse[]
+                    {
+                        new ErrorsResponse("188",
+                            "An unexpected error occurred",
+                            DateTime.Now)
+                    }
+                )
+            );
+        }
+    }
+}
